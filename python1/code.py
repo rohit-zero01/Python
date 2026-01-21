2c4738bbc8edf27e6ce7e6bb7ef7be0a86b13c55
Rohit Patil <rp7938008@gmail.com>
2026-01-21 23:18:43 +0530

    Add assignment programs

--- python1/code.py
+++ python1/code.py
@@ -39,3 +39,104 @@ avg = (a+b)/2
 
 print(avg)
 '''
+
+
+# #Assignment
+# #1.
+'''
+age = 21
+print("Hello Shradha, you are",age, "years old.")
+'''
+# #2.
+'''
+a = int(input("enter a:"))
+b = int(input("enter b:"))
+
+print("sum =", a+b)
+print("difference =", a-b)
+print("product =", a*b)
+print("quotient =", a%b)
+'''
+
+# #3.
+'''
+num2 = int(input("Enter the second integer: "))
+num3 = float(input("Enter a float: "))
+
+average = (float(num1) + float(num2) + float(num3)) / 3
+
+print(f"The average of the three numbers is: {average}")
+'''
+
+# #4.
+'''
+Taking input as a string
+a = input("Enter a: ")
+
+# Performing conversions
+val_int = int(a)
+val_float = float(a)
+val_string =(a)
+
+print(val_int, type(val_int))
+print(val_float, type(val_float))
+print(val_string, type(val_string))
+'''
+
+#5.
+'''
+x = 10+3*2**2
+
+print'''
+
+#6.
+'''
+a = input("enter a: ")
+b = input("enter b: ")
+
+a, b = b, a
+
+print(a)
+print(b)'''
+
+#7.
+'''
+temp = input("Enter temperature: ")
+
+CelsiusTemp = float(temp)
+
+Faren = (CelsiusTemp*(9/5))+32
+
+print(Faren)'''
+
+#8.
+'''
+r = input("Enter radius: ")
+radius = float(r)
+Area = 3.14*radius*radius
+
+print(Area)'''
+
+#9
+'''
+P = input("enter principle: ")
+R = input("enter rate: ")
+T = input("enter time: ")
+
+principle = float(P)
+rate = float(R)
+time = float(T)
+
+Simple_Interest = (principle*rate*time)/100
+
+print(Simple_Interest)'''
+
+#10
+'''
+num = float(input("enter num: "))
+
+i=int(num)
+f=num-i
+
+print(i)
+print(f)'''
