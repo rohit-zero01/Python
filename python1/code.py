51eac62bb4030efb2e7fd11d96f18d017bd3a095
Rohit Patil <rp7938008@gmail.com>
2026-01-21 23:17:54 +0530

    Add Python basics practice

---
+++ python1/code.py
@@ -0,0 +1,41 @@
+# # name = "rohit"
+# # age = 24
+# # PI = 3.14
+
+# # print(name,age,PI)
+'''
+num = 2
+isPrime = True
+
+print(type(isPrime))
+'''
+# #arithmetic operators
+'''
+a = 5
+b = 10 
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+'''
+# #user input
+'''
+a = int(input("enter a:"))
+b = int(input("enter b:"))
+
+sum = a+b
+
+print(sum)
+'''
+
+# #average
+'''
+a = int(input("enter a:"))
+b = int(input("enter b:"))
+
+avg = (a+b)/2
+
+print(avg)
+'''
